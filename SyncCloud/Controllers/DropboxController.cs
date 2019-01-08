201e521bffa08c39d2c1d0447b11d4c7a67366ba
b3kz0d <you@example.com>
2019-01-08 11:14:51 +0800

    Add Helper for Validate Signature and refactor DropboxController

--- SyncCloud/Controllers/DropboxController.cs
+++ SyncCloud/Controllers/DropboxController.cs
@@ -1,13 +1,10 @@
 ﻿using Newtonsoft.Json;
+using SyncCloud.Helpers;
 using SyncCloud.Models;
 using SyncCloud.Services;
-using System;
-using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -15,19 +12,30 @@ namespace SyncCloud.Controllers
 {
     public class DropboxController : Controller
     {
+        #region Fields
+
         private DropboxService _dropboxService;
+
+        #endregion
+
+        #region Constructor
+
         public DropboxController(DropboxService dropboxService)
         {
             _dropboxService = dropboxService;
         }
 
-        [System.Web.Mvc.HttpGet]
+        #endregion
+
+        #region Action Methods
+
+        [HttpGet]
         public ActionResult WebHook(string challenge)
         {
             return Content(challenge);
         
         }
-        [System.Web.Mvc.HttpPost]
+        [HttpPost]
         public ActionResult WebHook()
         {
             var signatureHeader = Request.Headers.GetValues("X-Dropbox-Signature");
@@ -39,50 +47,17 @@ namespace SyncCloud.Controllers
 
             string signature = signatureHeader.FirstOrDefault();
 
-            var isValid= ValidateSignature(content,signature);
+            var isValid= Helper.ValidateSignature(content,signature);
             if (!isValid)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             var model = JsonConvert.DeserializeObject<DropboxWebHookInputModel>(content);
             var task = Task.Run(() =>
             {
-                NotifyChanges(model);
+                _dropboxService.Sync(model);
             });
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
-        private void NotifyChanges(DropboxWebHookInputModel model)
-        {
-            _dropboxService.Sync(model);
-        }
-
-        private bool ValidateSignature(string content, string signature)
-        {
-            string appSecret = ConfigurationManager.AppSettings["Dropbox_ClientSecret"];
-            using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(appSecret)))
-            {
-                if (!VerifySha256Hash(hmac, content, signature))
-                    return false;
-            }
-            return true;
-        }
-
-        private string GetSha256Hash(HMACSHA256 sha256Hash, string input)
-        {
-            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-            var stringBuilder = new StringBuilder();
-            foreach (byte t in data)
-            {
-                stringBuilder.Append(t.ToString("x2"));
-            }
-            return stringBuilder.ToString();
-        }
-
-        private bool VerifySha256Hash(HMACSHA256 sha256Hash, string input, string hash)
-        {
-            string hashOfInput = GetSha256Hash(sha256Hash, input);
-            if (String.Compare(hashOfInput, hash, StringComparison.OrdinalIgnoreCase) == 0)
-                return true;
-            return false;
-        }
+        #endregion
     }
 }
---
+++ SyncCloud/Helpers/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SyncCloud.Helpers
+{
+    public static class Helper
+    {
+        public static bool ValidateSignature(string content, string signature)
+        {
+            string appSecret = ConfigurationManager.AppSettings["Dropbox_ClientSecret"];
+            using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(appSecret)))
+            {
+                if (!VerifySha256Hash(hmac, content, signature))
+                    return false;
+            }
+            return true;
+        }
+
+        public static string GetSha256Hash(HMACSHA256 sha256Hash, string input)
+        {
+            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var stringBuilder = new StringBuilder();
+            foreach (byte t in data)
+            {
+                stringBuilder.Append(t.ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static bool VerifySha256Hash(HMACSHA256 sha256Hash, string input, string hash)
+        {
+            string hashOfInput = GetSha256Hash(sha256Hash, input);
+            if (String.Compare(hashOfInput, hash, StringComparison.OrdinalIgnoreCase) == 0)
+                return true;
+            return false;
+        }
+    }
+}
